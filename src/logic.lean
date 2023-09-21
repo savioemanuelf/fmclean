a411a67180fffd0f01bc3e46660a68a7f8388f50
Sávio <savioemanuel2@gmail.com>
2023-09-21 17:29:25 -0300

    exists_as_neg_forall_converse

--- src/logic.lean
+++ src/logic.lean
@@ -567,7 +567,14 @@ end
 theorem exists_as_neg_forall_converse :
   ¬(∀x, ¬P x) → (∃x, P x)  :=
 begin
-  sorry,
+  intro h,
+  by_contra h',
+  apply h,
+  intro x,
+  intro px,
+  apply h',
+  existsi x,
+  exact px,
 end
 
 theorem forall_as_neg_exists_law :
